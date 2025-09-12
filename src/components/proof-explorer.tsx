acb2bc2f5d63e007d1eb6dbb1ac1e13b39df62b9
Phil Bogle <philbogle@gmail.com>
2025-09-12 07:09:54 +0000

    Still too much space below nav controls

--- src/components/proof-explorer.tsx
+++ src/components/proof-explorer.tsx
@@ -430,7 +430,7 @@ export default function ProofExplorer() {
       <div className="flex h-full min-h-screen w-full flex-col">
         <div className="w-full max-w-7xl mx-auto p-4 md:p-6 lg:p-8">
           <AppHeader />
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             <div className="md:col-span-1">
               <div className="sticky top-6 space-y-4">
                 <TheoremSelector
