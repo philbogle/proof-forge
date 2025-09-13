7fa2b9d97034624e12eac79c972870b90e4fadff
Phil Bogle <philbogle@gmail.com>
2025-09-13 13:12:09 +0000

    Do not let the navigation panel be to the left of the proof

--- src/components/proof-explorer.tsx
+++ src/components/proof-explorer.tsx
@@ -59,29 +59,25 @@ export default function ProofExplorer() {
   return (
     <TooltipProvider>
       <div className="flex h-full min-h-screen w-full flex-col">
-        <div className="w-full max-w-7xl mx-auto p-4 md:p-6 lg:p-8">
+        <div className="w-full max-w-4xl mx-auto p-4 md:p-6 lg:p-8">
           <AppHeader />
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div className="md:col-span-1">
-              <div className="sticky top-6 space-y-6">
-                <TheoremSelector
-                  theorems={theorems}
-                  selectedTheoremId={selectedTheoremId}
-                  onTheoremChange={handleTheoremChange}
-                />
-                <ProofControls
-                  formalityLevels={formalityLevels}
-                  formalityLevel={formalityLevel}
-                  isProofLoading={isProofLoading}
-                  onFormalityChange={handleFormalityChange}
-                  onRefresh={() => generateNewProof(true)}
-                  currentPage={currentPage}
-                  totalPages={proofPages.length}
-                  onPageChange={handlePageChange}
-                />
-              </div>
-            </div>
-            <div className="md:col-span-2 space-y-6">
+          <div className="space-y-6">
+            <TheoremSelector
+              theorems={theorems}
+              selectedTheoremId={selectedTheoremId}
+              onTheoremChange={handleTheoremChange}
+            />
+            <ProofControls
+              formalityLevels={formalityLevels}
+              formalityLevel={formalityLevel}
+              isProofLoading={isProofLoading}
+              onFormalityChange={handleFormalityChange}
+              onRefresh={() => generateNewProof(true)}
+              currentPage={currentPage}
+              totalPages={proofPages.length}
+              onPageChange={handlePageChange}
+            />
+            <div className="space-y-6">
               <ProofView
                 proof={
                   !renderMarkdown && user
