b59c24f32624076343968cfb804876c683839aa4
Phil Bogle <philbogle@gmail.com>
2025-09-14 23:00:35 +0000

    Make the vertical padding around the navigation buttons much tighter

--- src/components/proof-explorer/proof-controls.tsx
+++ src/components/proof-explorer/proof-controls.tsx
@@ -30,7 +30,7 @@ export default function ProofControls({
 }: ProofControlsProps) {
 
   return (
-    <div className="space-y-4">
+    <div className="">
       <div className="flex items-center justify-between gap-2">
         <div className="flex flex-col items-stretch gap-2 rounded-lg border bg-card p-1">
           <div className="flex items-center gap-1">
