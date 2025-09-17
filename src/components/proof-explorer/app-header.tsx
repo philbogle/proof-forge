4f08148caf5c7974cf266f2d2ec3ce180129ffcf
Phil Bogle <philbogle@gmail.com>
2025-09-17 13:03:05 +0000

    we want about to appear even for signed out users.  How about having the

--- src/components/proof-explorer/app-header.tsx
+++ src/components/proof-explorer/app-header.tsx
@@ -14,7 +14,7 @@ import {
   DropdownMenuLabel,
 } from '@/components/ui/dropdown-menu';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { LogIn, LogOut, Shield, ChevronDown, Info } from 'lucide-react';
+import { LogIn, LogOut, Shield, ChevronDown, Info, User } from 'lucide-react';
 import { Skeleton } from '../ui/skeleton';
 import { isAdmin } from '@/lib/auth';
 import Link from 'next/link';
@@ -56,49 +56,71 @@ export default function AppHeader({ onToggleEditing }: AppHeaderProps) {
       <div>
         {loading ? (
           <Skeleton className="h-9 w-24 rounded-md" />
-        ) : user ? (
+        ) : (
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="ghost">
-                <Avatar className="h-6 w-6 mr-2">
-                    <AvatarImage src={user.photoURL || ''} alt={user.displayName || 'User'} />
-                    <AvatarFallback>
-                        {user.displayName ? user.displayName.charAt(0) : '?'}
-                    </AvatarFallback>
-                </Avatar>
-                <span className="hidden md:inline">{user.displayName}</span>
-                <ChevronDown className="ml-2 h-4 w-4" />
-              </Button>
+                { user ? (
+                    <Button variant="ghost">
+                        <Avatar className="h-6 w-6 mr-2">
+                            <AvatarImage src={user.photoURL || ''} alt={user.displayName || 'User'} />
+                            <AvatarFallback>
+                                {user.displayName ? user.displayName.charAt(0) : '?'}
+                            </AvatarFallback>
+                        </Avatar>
+                        <span className="hidden md:inline">{user.displayName}</span>
+                        <ChevronDown className="ml-2 h-4 w-4" />
+                    </Button>
+                ) : (
+                    <Button variant="ghost" size="icon">
+                        <Avatar className="h-8 w-8">
+                            <AvatarFallback>
+                                <User className='h-5 w-5' />
+                            </AvatarFallback>
+                        </Avatar>
+                    </Button>
+                )}
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              {isUserAdmin && (
-                <DropdownMenuItem asChild>
-                  <Link href="/admin">
-                    <Shield className="mr-2 h-4 w-4" />
-                    <span>Admin</span>
-                  </Link>
-                </DropdownMenuItem>
-              )}
-               <DropdownMenuItem asChild>
-                  <Link href="/about">
-                    <Info className="mr-2 h-4 w-4" />
-                    <span>About</span>
-                  </Link>
-                </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={handleSignOut}>
-                <LogOut className="mr-2 h-4 w-4" />
-                <span>Sign Out</span>
-              </DropdownMenuItem>
+                { user ? (
+                    <>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        {isUserAdmin && (
+                            <DropdownMenuItem asChild>
+                            <Link href="/admin">
+                                <Shield className="mr-2 h-4 w-4" />
+                                <span>Admin</span>
+                            </Link>
+                            </DropdownMenuItem>
+                        )}
+                        <DropdownMenuItem asChild>
+                            <Link href="/about">
+                                <Info className="mr-2 h-4 w-4" />
+                                <span>About</span>
+                            </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem onClick={handleSignOut}>
+                            <LogOut className="mr-2 h-4 w-4" />
+                            <span>Sign Out</span>
+                        </DropdownMenuItem>
+                    </>
+                ) : (
+                    <>
+                        <DropdownMenuItem onClick={handleSignIn}>
+                            <LogIn className="mr-2 h-4 w-4" />
+                            <span>Sign In</span>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                            <Link href="/about">
+                                <Info className="mr-2 h-4 w-4" />
+                                <span>About</span>
+                            </Link>
+                        </DropdownMenuItem>
+                    </>
+                )}
             </DropdownMenuContent>
           </DropdownMenu>
-        ) : (
-            <Button variant="outline" onClick={handleSignIn}>
-                <LogIn className="mr-2 h-4 w-4" />
-                Sign In
-            </Button>
         )}
       </div>
     </header>
