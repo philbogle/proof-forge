a177c4f748c982c40e96b9bf094690fcc44d9566
Phil Bogle <philbogle@gmail.com>
2025-09-18 15:22:00 +0000

    Make sure the placeholder a larger holder for the proof title followed b

--- src/components/proof-explorer/proof-view.tsx
+++ src/components/proof-explorer/proof-view.tsx
@@ -31,11 +31,23 @@ const ProofView = React.forwardRef<HTMLDivElement, ProofViewProps>(
           {isGenerating ? (
             <ProofLoadingIndicator />
           ) : showLoadingSkeleton ? (
-             <div className="space-y-4 w-full">
-                <Skeleton className="h-6 w-3/4" />
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-5/6" />
-                <Skeleton className="h-4 w-full" />
+             <div className="space-y-6 w-full">
+                <Skeleton className="h-8 w-4/5" />
+                <div className="space-y-3">
+                    <Skeleton className="h-4 w-full" />
+                    <Skeleton className="h-4 w-full" />
+                    <Skeleton className="h-4 w-11/12" />
+                </div>
+                <div className="space-y-3">
+                    <Skeleton className="h-4 w-full" />
+                    <Skeleton className="h-4 w-10/12" />
+                </div>
+                <div className="space-y-3">
+                    <Skeleton className="h-4 w-full" />
+                    <Skeleton className="h-4 w-full" />
+                    <Skeleton className="h-4 w-full" />
+                    <Skeleton className="h-4 w-3/4" />
+                </div>
              </div>
           ) : (
              <div
